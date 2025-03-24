1a13514d7f2f39e8ce4666269d1a2c7eec57b0d1
gilba <gilba@GIL>
2025-03-24 13:33:31 +0000

    task2

---
+++ user/memsize_test.c
@@ -0,0 +1,14 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define KB 1024
+
+int main(int argc, char *argv[]) {
+    printf("memsize: %d\n", memsize());
+    int *p = malloc(20000);
+    printf("memsize: %d\n", memsize());
+    free(p);
+    printf("memsize: %d\n", memsize());
+    exit(0);
+}
