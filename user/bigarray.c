b0e068e8ff3a1004686e972de0a45eb60c626973
gilba <gilba@GIL>
2025-04-02 12:12:03 +0000

    fixed not divisible number of childern

--- user/bigarray.c
+++ user/bigarray.c
@@ -3,14 +3,14 @@
 #include "user/user.h"
 
 #define N 65536 // 2^16
-#define NUM_CHILDREN 4
+#define NUM_CHILDREN 13
 int
 main(int argc, char *argv[])
 {
     int *array = malloc(N * sizeof(int));
     if(!array) {
         printf("malloc failed\n");
-        exit(1);
+        exit(1, "malloc failed");
     }
     int pids[NUM_CHILDREN];
     int n_finished;
@@ -25,14 +25,22 @@ main(int argc, char *argv[])
     if (proc_num < 0) {
         free(array);
         printf("forkn failed\n");
-        exit(1);
+        exit(1, "forkn failed");
     }
 
     if(proc_num > 0) {
         // Calculate the child's portion
         int portion = N / NUM_CHILDREN;
+        int remainder = N % NUM_CHILDREN;
         int start = portion * (proc_num - 1);
-        int end = start + portion;
+        int end;
+        
+        // Last child handles the remainder
+        if (proc_num == NUM_CHILDREN) {
+            end = start + portion + remainder;
+        } else {
+            end = start + portion;
+        }
         int sum = 0;
 
         // Sum this portion
@@ -40,15 +48,15 @@ main(int argc, char *argv[])
             sum += array[i];
         }
         free(array);
-        printf("Child %d, Sum = %d\n", proc_num, sum);
-        exit(sum);
+        printf("Child %d, Sum = %d (range: %d-%d)\n", proc_num, sum, start, end-1);
+        exit(sum, "Child summed");
     }
     else {
         // Wait for all children to finish
         if(waitall(&n_finished, statuses) < 0) {
             free(array);
             printf("waitall failed\n");
-            exit(1);
+            exit(1, "waitall failed");
         }
 
         // Sum all the children's sums
@@ -58,7 +66,7 @@ main(int argc, char *argv[])
         }
         free(array);
         printf("Total Sum = %d\n", total);
-        exit(0);
+        exit(0, "Parent summed");
     }
 
 }
